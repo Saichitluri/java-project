9eef1b0f86308152d46c063cefe96706f87d8c41
sai <saichitluri98@gmail.com>
2026-09-12 13:24:26 -0700

    modified ARN

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -45,7 +45,7 @@ resource "aws_iam_role" "beanstalk_role" {
 
 resource "aws_iam_role_policy_attachment" "beanstalk_role_attachment" {
   role       = aws_iam_role.beanstalk_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSelasticBeanstalkEnhancedHealth"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkEnhancedHealth"
 }
 
 resource "aws_iam_role_policy_attachment" "beanstalk_role_attachment2" {
